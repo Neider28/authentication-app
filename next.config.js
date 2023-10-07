08168d341e7f1cfe403833da72c8be43aba9fe0b
Neider28 <neidersilva2802@gmail.com>
2023-10-07 16:01:13 -0500

    Image URL ✨

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'auth-backend-smoky.vercel.app',
+        hostname: 'res.cloudinary.com',
         port: '',
       },
     ],
